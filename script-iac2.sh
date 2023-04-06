3c3270d5ed1921bc6b5241a72e72166eb2d25904
billuzumaki <santos-emiliano@hotmail.com>
2023-04-06 11:01:18 +0000

    apache version 1.0

---
+++ script-iac2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "atualiando sistema"
+
+apt update -y
+apt upgrade -y
+
+echo "baixando servidor apache e unzip"
+apt install apache2 -y
+apt install unzip -y
+
+echo "desconpaquitando arquivo"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip 
+unzip main.zip
+
+echo "copiando arquivos"
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "finalizado"
+
